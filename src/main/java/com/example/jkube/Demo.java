c35f3e78528ab64367db0c09944ffea6615442c6
Marc Nuri <marc@marcnuri.com>
2023-07-17 09:59:14 +0200

    chore: fix format

--- src/main/java/com/example/jkube/Demo.java
+++ src/main/java/com/example/jkube/Demo.java
@@ -6,14 +6,14 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class Demo {
-	
-	@Value("${welcome}")
-    private String welcome;
 
-    @GetMapping("/hello")
-    public String hello() {
-        return welcome;
-    }
-	
+	@Value("${welcome}")
+		private String welcome;
+
+		@GetMapping("/hello")
+		public String hello() {
+			return welcome;
+		}
+
 
 }
